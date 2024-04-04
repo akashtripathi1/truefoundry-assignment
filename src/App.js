3f863310907df87365aa66cad320be6df68d8acd
akashtripathi1 <120573423+akashtripathi1@users.noreply.github.com>
2024-04-04 18:17:53 +0530

    deleted unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 // App.js
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes , Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Logs from './components/logs/LogContainer';
 import Metrics from './components/metrics/MetricsContainer';
 import { TimeRangeProvider } from './context/TimeRangeContext';
 import { LiveFeedStatusProvider } from './context/LiveFeedStatusContext';
 import './App.css';
+import Storybook from './components/Storybook';
 
 
 function App() {
   return (
     <Router>
       <LiveFeedStatusProvider>
-      <TimeRangeProvider> 
-      <div className='app'>
-        
-        <Navbar title = 'truefoundry' icon= 'TFlogo' />
-        <Routes>
-          <Route exact path="/metrics" element={(
-            <Fragment>
-              <Metrics />
-            </Fragment>
-          )} />
-          <Route exact path="/logs" element={(
-            <Fragment>
-              <Logs />
-            </Fragment>
-          )} />
-          <Route exact path="/storybook" element={<Navigate to = "/storybook-static/index.html" replace />} />
+        <TimeRangeProvider>
+          <div className='app'>
 
-        </Routes>
-
-      </div>
-      </TimeRangeProvider>
+            <Navbar title='truefoundry' icon='TFlogo' />
+            <Routes>
+              <Route exact path="/metrics" element={(
+                <Fragment>
+                  <Metrics />
+                </Fragment>
+              )} />
+              <Route exact path="/logs" element={(
+                <Fragment>
+                  <Logs />
+                </Fragment>
+              )} />
+              <Route exact path="/storybook" element={
+                <Fragment>
+                  {/* <Navigate replace to="/storybook-static"/> */}
+                  <Storybook />
+                </Fragment>} />
+            </Routes>
+          </div>
+        </TimeRangeProvider>
 
       </LiveFeedStatusProvider>
     </Router>
---
+++ src/components/Storybook.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Storybook = () => {
+return(
+    <a href="https://react-metrics-logs-dashboard.netlify.app/storybook-static/index.html">Storybook</a>
+);
+}
+export default Storybook;
